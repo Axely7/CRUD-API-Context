61b038c85db5b6f1355947b1571e5ff6e4d87981
Axely7 <axely7@hotmail.com>
2022-06-23 13:18:54 -0500

    Creating form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TaskForm = () => {
+
+  const [task, setTask] = useState({
+    title: '',
+    description: ''
+  })
+
+  const handleChange = e => {
+    setTask({...task, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(task);
+  }
+
   return (
-    <div>TaskForm</div>
+    <div className='flex justify-center items-center h-3/4'>
+      <form className='bg-gray-900 p-10' onSubmit={handleSubmit}>
+        <h2 className='mb-7 text-4xl'>A Task</h2>
+        <div className='mb-5'>
+          <input 
+            type="text" 
+            name='title' 
+            placeholder='Write a title' 
+            onChange={handleChange}
+            className='py-3 px-4 focus:outline-none focus:text-gray-100 bg-gray-700 w-full'/>
+        </div>
+        <div className='mb-5'>
+          <textarea 
+            name='description' 
+            rows="2" 
+            placeholder='Write a description' 
+            onChange={handleChange}
+            className='py-3 px-4 focus:outline-none focus:text-gray-100 bg-gray-700 w-full'
+            ></textarea>
+        </div>
+        <button className='bg-green-600 w-full hover:bg-green-500 py-2 px-4 mt-5'>
+          Create Task
+        </button>
+      </form>
+    </div>
   )
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -9,12 +9,24 @@ const TaskList = () => {
 
   return (
     <div className='flex justify-center'>
-      { tasks.map( task => (
-        <div className='bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between'>
-          <h1 className=''>{task.title}</h1>
-          <h6>{ task.id }</h6>
-        </div>
-      ) ) }
+      <div className='w-6/12'>
+        { tasks.map( task => (
+          <div className='bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between' key={task.id}>
+            <div>
+              <h1 className=''>{task.title}</h1>
+              <h6>{ task.id }</h6>
+            </div>
+            <div>
+              <button>
+                Edit
+              </button>
+              <button>
+                Delete
+              </button>
+            </div>
+          </div>
+        ) ) }
+      </div>
     </div>
   )
 }
